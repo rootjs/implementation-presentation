75f12fea2117f29c5e1013a436f1fc5ba09c53e5
Christoph Wolff <icydar@hotmail.com>
2016-02-14 01:17:09 +0100

    Small fixes

--- src/graphdemo.js
+++
@@ -1,3 +0,0 @@
-var root = require('/Users/christoph/code/rootjs/implementation/index.js');
-
-root.gCling.LoadFile("/Users/christoph/code/rootjs/implementation-presentation/src/exclusiongraph.C");
--- src/guidemo.js
+++ src/guidemo.js
@@ -1,4 +1,3 @@
 var root = require('/Users/christoph/code/rootjs/implementation/index.js');
 
 new root.TBrowser();
-
